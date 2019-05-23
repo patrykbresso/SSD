d50fd7a4e1a4aea0f80891e8be051e4d2fe0f5f6
patrykbresso <brychu15@gmail.com>
2019-05-23 21:43:24 +0200

    Init

---
+++ src/test/java/com/ssdproject/example/SSD/SsdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ssdproject.example.SSD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
