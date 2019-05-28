30caef6f61d0d4c3ca141daf8a248a5daeb6bc73
Judyta Gołdon <218300@student.pwr.edu.pl>
2019-05-28 20:40:11 +0200

    new components

---
+++ client/src/app/conference/conference-details/conference-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conference-details',
+  templateUrl: './conference-details.component.html',
+  styleUrls: ['./conference-details.component.css']
+})
+export class ConferenceDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/conference/conferences-overview/conferences-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conferences-overview',
+  templateUrl: './conferences-overview.component.html',
+  styleUrls: ['./conferences-overview.component.css']
+})
+export class ConferencesOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
