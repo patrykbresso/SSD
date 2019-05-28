0929c0ca45bf8926ad654d28f0ab13a1c801b045
Judyta Gołdon <218300@student.pwr.edu.pl>
2019-05-28 20:39:10 +0200

    style changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,13 +11,29 @@ import { AppModuleRouting } from './app.module.routing';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { httpInterceptorProviders } from './interceptor/auth.interceptor';
 import { AuthStorageService } from './auth/service/auth-storage.service';
+import { ConferenceDetailsComponent } from './conference/conference-details/conference-details.component';
+import { ConferencesOverviewComponent } from './conference/conferences-overview/conferences-overview.component';
+import { PaymentComponent } from './payment/payment.component';
+import { RegisterExhItemComponent } from './register-exh-item/register-exh-item.component';
+import { ReviewComponent } from './review/review.component';
+import { UploadComponent } from './upload/upload.component';
+import { UploadPosterComponent } from './upload-poster/upload-poster.component';
+import { UploadPresentationComponent } from './register-exh-item/upload-presentation/upload-presentation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TabbarComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ConferenceDetailsComponent,
+    ConferencesOverviewComponent,
+    PaymentComponent,
+    RegisterExhItemComponent,
+    ReviewComponent,
+    UploadComponent,
+    UploadPosterComponent,
+    UploadPresentationComponent
   ],
   imports: [
     BrowserModule,
