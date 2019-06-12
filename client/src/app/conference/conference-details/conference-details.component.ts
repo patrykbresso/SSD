36836026a1bd9efc377c3ab2bfcaca69bd6e2ea4
Judyta Gołdon <218300@student.pwr.edu.pl>
2019-06-12 20:39:20 +0200

    overview of presentation details fixed

--- client/src/app/conference/conference-details/conference-details.component.ts
+++ client/src/app/conference/conference-details/conference-details.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {SimpleConference} from "../simple-conference";
 import {Conference} from "../conference";
+import {Schedule} from "../schedule";
+import {ScheduleItem} from "../schedule-item";
+import {formatDate} from "ngx-bootstrap";
+import {Author} from "../author";
 
-//TODO add all authors
 @Component({
   selector: 'app-conference-details',
   templateUrl: './conference-details.component.html',
@@ -13,6 +15,7 @@ export class ConferenceDetailsComponent implements OnInit {
 
   data: any;
   conference: Conference;
+  scheduleItems: ScheduleItem[];
 
   constructor(
     private route: ActivatedRoute
@@ -21,8 +24,28 @@ export class ConferenceDetailsComponent implements OnInit {
   ngOnInit() {
     this.data = this.route.snapshot.data;
     this.conference = this.data.details;
+    this.scheduleItems = this.conference.schedule.presentationsAndPosters;
   }
 
   headElements = ['Presentation Title', 'Author', 'Start Date', 'Start Time', 'Duration', 'Location'];
 
+  getAuthors(scheduleItem: ScheduleItem): string {
+    let authors: Author[] = [];
+    if (scheduleItem.presentation !== null && scheduleItem.presentation !== undefined) {
+      scheduleItem.presentation.authors.forEach(a => authors.push(a));
+    }
+    if (scheduleItem.posters !== null && scheduleItem.posters !== undefined) {
+      scheduleItem.posters.forEach(p => p.authors.forEach(a => authors.push(a)));
+    }
+    let authorsString = '';
+    authors.forEach(a => authorsString += (a.academicTitle + ' ' + a.name + ' ' + a.surname + ', '));
+    return authorsString.slice(0, authorsString.length - 2);
+  }
+
+  getDuration(scheduleItem: ScheduleItem) {
+    let start = Date.parse(scheduleItem.startDate);
+    let end = Date.parse(scheduleItem.endDate);
+    let diff = Math.floor(((end - start) % 86400000) / 3600000);
+    return diff.toString() + ' h';
+  }
 }
